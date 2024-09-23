ea09b10eeb5c3b3a5aca4323d9e8169d504b9e43
Jorge Ramirez <ramirezj.jorge@gmail.com>
2024-09-23 11:03:09 -0300

    Bug fixing
    Fixed erroneous csv writing

--- PreProcessAndOCR.py
+++ PreProcessAndOCR.py
@@ -33,7 +33,9 @@ def ocr_stat(imagen):
     for renglon in resultado:
         sumaTasa += renglon[2]
         nfilas+=1
-        promedio=sumaTasa/nfilas
+    promedio=sumaTasa/nfilas
+    if nfilas==0 :
+        nfilas=1
     print ("Suma de las tasas: ",sumaTasa)
     print("tasa promedio: ",sumaTasa/nfilas)
     return promedio
@@ -46,20 +48,23 @@ lector=easyocr.Reader(["es"],gpu=False)
 imagen=cv2.imread(ruta,cv2.IMREAD_GRAYSCALE)
 
 # OCR inicial. 
+print("Valores iniciales: ")
 promedioInicial=ocr_stat(imagen)
 
 # Aplicar CLAHE, queda en imagen2
 clahe=cv2.createCLAHE(clipLimit=5)
 imagen2=clahe.apply(imagen)
 
+print("Aplicando CLAHE")
+promeCLAHE=ocr_stat(imagen2)
+
 # máscara de desenfoque sobre imagen2, queda en sharpened
 
 kernel = np.array([[0, -1, 0],[-1, 5, -1],[0, -1, 0]])
 sharpened = cv2.filter2D(imagen2, -1, kernel)
 
-# definir el nombre de archivo
-pospunto=ruta.find('.')
-nombre=ruta[:pospunto]+'2.png'
+print("Aplicando máscara de desenfoque")
+promeSharpening=ocr_stat(sharpened)
 
 # Binarización Otsu
 
@@ -67,15 +72,21 @@ gaussiano=cv2.GaussianBlur(sharpened, (5,5),0)
 ret,binarizado=cv2.threshold(gaussiano,0,255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 #OCR final
+print("Aplicando binarización OTSU")
 promefinal= ocr_stat(binarizado)
 
 #agregar valores a evolOCR.csv
-with open('evolOCR.csv',mode="a") as arcsv:
-    renglon=ruta+','+str(promedioInicial)+','+str(promefinal)
+with open('tasasOCR.csv',mode="a") as arcsv:
+    #renglon=ruta+','+str(promedioInicial)+','+str(promeCLAHE)+','+str(promeSharpening)+','+str(promefinal)
+    fila=[ruta,str(promedioInicial),str(promeCLAHE),str(promeSharpening),str(promefinal)]
     escritor=csv.writer(arcsv)
-    escritor.writerow(renglon)
+    escritor.writerow(fila)
 
 ultima=binarizado
+# definir el nombre de archivo a guardar
+pospunto=ruta.find('.')
+nombre=ruta[:pospunto]+'2.png'
+
 # Juntar la imagen original y la última y guardarlas
 res=np.hstack((imagen,ultima))
 cv2.imwrite(nombre,res)
